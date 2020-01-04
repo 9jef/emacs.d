924c4e65e7a3e7584956f9849f70dfb0e7f57c95
J E F <fletcherje88@gmail.com>
2020-01-04 14:36:40 +0000

    added the files that matter..

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (skeletor persistent-scratch ido-completing-read ido-complete-space-or-hyphen ido-completing-read* ido-completing-read+ counsel slime-company paren-face paredit ivy-hydra swiper magit flycheck company moe-theme systemd c-mode emacs-lisp-mode haskell-mode markdown-mode common-lisp-mode json-mode cmake-mode lisp-mode fic-mode diff-hl which-key undo-tree smooth-scrolling projectile flx-ido ido-vertical-mode ido-ubiquitous smex smart-mode-line better-defaults diminish use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
